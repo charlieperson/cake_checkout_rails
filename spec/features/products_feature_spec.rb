b094b4f7f44e89de125c12b9038b4c3106339ffd
AlanGabbianelli <alan.gabbianelli@gmail.com>
2016-05-27 11:35:32 +0100

    Show existing product

--- spec/features/products_feature_spec.rb
+++ spec/features/products_feature_spec.rb
@@ -7,4 +7,12 @@ feature 'products' do
       expect(page).to have_content 'No products in stock'
     end
   end
+
+  context 'products have been added in stock' do
+    scenario 'should show available products' do
+      Product.create(code: 001, name: 'Lavander heart', price: 9.25)
+      visit '/products'
+      expect(page).to have_content 'Lavander heart'
+    end
+  end
 end
